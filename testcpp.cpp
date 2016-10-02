6ba0b9cf1190e452066d5b3f855dcde54e7c7a9d
Petter Taule <pettertaule@live.no>
2016-10-03 00:27:10 +0200

    La til test-python og -cpp filer

---
+++ testcpp.cpp
@@ -0,0 +1,17 @@
+/*
+   testcpp.cpp
+
+   Created by Petter Taule on 02.10.2016
+   Copyright (c) 2016 Petter Taule. All rights reserved.
+*/
+
+
+#include <iostream>
+
+int main() {
+    int a = 3;
+    int b = 4;
+    for (int i = 0; i < (a+b); i++) {
+        std::cout << "Hello world!" << std::endl;
+    }
+}
---
+++ testpython.py
@@ -0,0 +1,6 @@
+
+a = 3
+b = 4
+
+for i in range(0,a+b):
+    print ("Hello world!")
